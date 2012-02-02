59923cc88392299b5f5e50e53bc69fcdb30d11de
Ryan McCue <me@ryanmccue.info>
2012-02-02 19:27:30 +1000

    Twitter is now on by default

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ admins = [owner,'alphabeat','MonkeyCrumpets']
 # If you want to enumerate a list of modules rather than disabling
 # some, use "enable = ['example']", which takes precedent over exclude
 # 
-enable = ['startup', 'admin', 'reload', 'tell', 'head', 'wikipedia', 'seen', 'reddit', 'youtube', 'cleverircbot', 'unshorten']
+enable = ['startup', 'admin', 'reload', 'tell', 'head', 'wikipedia', 'seen', 'reddit', 'youtube', 'cleverircbot', 'unshorten', 'twitter']
 
 # EOF
