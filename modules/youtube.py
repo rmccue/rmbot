38b5f60bfbd965ce14e01605b6b349cbac32d2e4
Ryan McCue <me@ryanmccue.info>
2012-09-01 00:17:56 +1000

    Reformat the YouTube module

--- modules/youtube.py
+++ modules/youtube.py
@@ -16,34 +16,41 @@ yt_service = gdata.youtube.service.YouTubeService()
 # The YouTube API does not currently support HTTPS/SSL access.
 yt_service.ssl = False
 
+
 def group(number):
-    s = '%d' % int(number)
-    groups = []
-    while s and s[-1].isdigit():
-        groups.append(s[-3:])
-        s = s[:-3]
-    return s + ','.join(reversed(groups))
+	s = '%d' % int(number)
+	groups = []
+	while s and s[-1].isdigit():
+			groups.append(s[-3:])
+			s = s[:-3]
+	return s + ','.join(reversed(groups))
+
 
 def link(id):
-   try:
-      entry = yt_service.GetYouTubeVideoEntry(video_id=id)
-      if entry:
-         #return "'{0}' - {1} views - {2}".format(entry.media.title.text, group(entry.statistics.view_count), entry.media.player.url.replace('&feature=youtube_gdata_player', ''))
-         if entry.statistics:
-            return "'{0}' - {1} views - http://youtu.be/{2}".format(entry.media.title.text, group(entry.statistics.view_count), id)
-         return "'{0}' - {1} views - http://youtu.be/{2}".format(entry.media.title.text, 0, id)
-      return None
-   except gdata.service.RequestError:
-         return "That video doesn't exist, you Monkey-esque moron. Almost as bad as alphabeat!"
+	try:
+		entry = yt_service.GetYouTubeVideoEntry(video_id=id)
+		if entry:
+			#return "'{0}' - {1} views - {2}".format(entry.media.title.text, group(entry.statistics.view_count), entry.media.player.url.replace('&feature=youtube_gdata_player', ''))
+			if entry.statistics:
+					return "'{0}' - {1} views - http://youtu.be/{2}".format(entry.media.title.text, group(entry.statistics.view_count), id)
+			return "'{0}' - {1} views - http://youtu.be/{2}".format(entry.media.title.text, 0, id)
+		return None
+	except gdata.service.RequestError:
+		return "That video doesn't exist, you Monkey-esque moron. Almost as bad as alphabeat!"
 uri_matcher = re.compile(r'http://(?:www\.)?youtu(?:\.be/|be\.com/watch\?v=)([^&#\s]+)')
-def yt_matcher(bot, input): 
-   matches = uri_matcher.search(input.group(1))
-   if not matches:
-      return
 
-   bot.say(link(matches.group(1)))
+
+def yt_matcher(bot, input):
+	"""Gives information on YouTube links"""
+	matches = uri_matcher.search(input.group(1))
+	if not matches:
+		return
+
+	bot.say(link(matches.group(1)))
+yt_matcher.name = 'YouTube Autolinker'
 yt_matcher.rule = r'.*(http://(?:www\.)?youtu(?:\.be|be\.com)/.*)'
 yt_matcher.priority = 'low'
 
-if __name__ == '__main__': 
-   print __doc__.strip()
+
+if __name__ == '__main__':
+	print __doc__.strip()
