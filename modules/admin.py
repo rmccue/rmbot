1e7f993b6145ddb641c20f47ecdf708c4c1d9a42
Nick Perkins <nick@nperkins.net>
2012-11-25 20:41:00 +1000

    Fixed unicode issues in admin module Updated config sample file

--- modules/admin.py
+++ modules/admin.py
@@ -11,7 +11,7 @@ http://inamidst.com/bot/
 def join(bot, input):
 	"""Join the specified channel. This is an admin-only command."""
 	# Can only be done in privmsg by an admin
-	if input.sender.startswith('#') and not input.sender == '#rmccue':
+	if input.sender.startswith('#') and not input.sender == bot.config.adminchan:
 		return
 	if input.admin:
 		channel, key = input.group(2), input.group(3)
@@ -27,10 +27,10 @@ join.example = '.join #example or .join #example key'
 def part(bot, input):
 	"""Part the specified channel. This is an admin-only command."""
 	# Can only be done in privmsg by an admin
-	if input.sender.startswith('#') and not input.sender == '#rmccue':
+	if input.sender.startswith('#') and not input.sender == bot.config.adminchan:
 		return
 	if input.admin:
-		bot.part(input.group(2))
+		bot.part(input.group(2).encode('utf-8'))
 part.commands = ['part']
 part.priority = 'low'
 part.example = '.part #example'
@@ -39,7 +39,7 @@ part.example = '.part #example'
 def quit(bot, input):
 	"""Quit from the server. This is an owner-only command."""
 	# Can only be done in privmsg by the owner
-	if input.sender.startswith('#') and not input.sender == '#rmccue':
+	if input.sender.startswith('#') and not input.sender == bot.config.adminchan:
 		return
 	if input.owner:
 		bot.quit()
@@ -49,17 +49,17 @@ quit.priority = 'low'
 
 def msg(bot, input):
 	# Can only be done in privmsg by an admin
-	if input.sender.startswith('#') and not input.sender == '#rmccue':
+	if input.sender.startswith('#') and not input.sender == bot.config.adminchan:
 		return
 	if input.admin:
-		bot.msg(input.group(2), input.group(3))
+		bot.msg(input.group(2).encode('utf-8'), input.group(3))
 msg.rule = (['msg'], r'(#?\S+) (.*)')
 msg.priority = 'low'
 
 
 def me(bot, input):
 	# Can only be done in privmsg by an admin
-	if input.sender.startswith('#') and not input.sender == '#rmccue':
+	if input.sender.startswith('#') and not input.sender == bot.config.adminchan:
 		return
 	if input.admin:
 		bot.action(input.group(2).encode('utf-8'), input.group(3))
