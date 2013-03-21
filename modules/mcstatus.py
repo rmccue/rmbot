9d732a8e1db3080437569c2979efce56314771fc
ThePengwin <articuspengwin@gmail.com>
2013-03-21 17:05:02 +1000

    Cleanup to code

--- modules/mcstatus.py
+++ modules/mcstatus.py
@@ -4,29 +4,36 @@ import web
 import json
 
 def getstatus():
-	body = web.get(checkurl)
-	result = json.loads(body)
-	return result['report']
+        body = web.get(checkurl)
+        result = json.loads(body)
+        return result['report']
 
 
 def mcstatus(bot, input):
 
-	try:
-		deadservers = []
+        try:
+                deadservers = []
 
-		results = getstatus()
-		for server in results:
-			if results[server]['status'] != 'up':
-				deadservers.append(server)
-		
-		if len(deadservers) == 0:
-			bot.reply('Servers seem fine to me :)')
-		else:
-			bot.reply('Looks like there could be problems with '+', '.join(deadservers)+'. See more at http://xpaw.ru/mcstatus/')
-			
+                results = getstatus()
 
-	except e:
-		bot.reply('Erm, something broke :/')
+                for server in results:
+
+                        if results[server]['status'] != 'up':
+
+                                deadservers.append(server)
+
+                if len(deadservers) == 0:
+
+                        bot.reply('Servers seem fine to me :)')
+
+                else:   
+                        deadserverlist = ', '.join(deadservers)
+
+                        bot.reply('Looks like there could be problems with ' + deadserverlist + '. See more at http://xpaw.ru/mcstatus/')
+
+
+        except e:
+                bot.reply('Erm, something broke :/')
 
 mcstatus.commands = ['mcstatus']
 
